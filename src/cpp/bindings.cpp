c517ebe00f18f50e1846ec01ec71b4de066d7c6e
Vivekananda Giri <147533720+vivekananda05@users.noreply.github.com>
2026-02-05 19:27:56 +0530

    Add files via upload

---
+++ src/cpp/bindings.cpp
@@ -0,0 +1,228 @@
+// #include <pybind11/pybind11.h>
+// #include <pybind11/stl.h>
+// #include <pybind11/numpy.h>
+// #include "tensor.h"
+// #include "layers.h"
+// #include "optimizers.h"
+// #include "utils.h"
+
+// namespace py = pybind11;
+
+// PYBIND11_MODULE(custom_dl_framework, m) {
+//     m.doc() = "Custom Deep Learning Framework";
+    
+//     // Tensor class
+//     py::class_<Tensor>(m, "Tensor")
+//         .def(py::init<>())
+//         .def(py::init<const std::vector<int>&, bool>(), 
+//              py::arg("shape"), py::arg("requires_grad") = false)
+//         .def(py::init<const std::vector<float>&, const std::vector<int>&, bool>(),
+//              py::arg("data"), py::arg("shape"), py::arg("requires_grad") = false)
+//         .def("getShape", &Tensor::getShape)
+//         .def("getData", &Tensor::getData)
+//         .def("getTotalSize", &Tensor::getTotalSize)
+//         .def("zeros", &Tensor::zeros)
+//         .def("ones", &Tensor::ones)
+//         .def("randomNormal", &Tensor::randomNormal, 
+//              py::arg("mean") = 0.0f, py::arg("stddev") = 1.0f)
+//         .def("randomUniform", &Tensor::randomUniform,
+//              py::arg("low") = 0.0f, py::arg("high") = 1.0f)
+//         .def("xavierUniform", &Tensor::xavierUniform)
+//         .def("matmul", &Tensor::matmul)
+//         .def("conv2d", &Tensor::conv2d)
+//         .def("maxPool2d", &Tensor::maxPool2d)
+//         .def("relu", &Tensor::relu)
+//         .def("sigmoid", &Tensor::sigmoid)
+//         .def("softmax", &Tensor::softmax, py::arg("axis") = -1)
+//         .def("reshape", &Tensor::reshape)
+//         .def("flatten", &Tensor::flatten)
+//         .def("backward", &Tensor::backward)
+//         .def("zeroGrad", &Tensor::zeroGrad)
+//         .def("__repr__", &Tensor::toString)
+//         .def("__add__", [](const Tensor& a, const Tensor& b) {
+//             return a.elementwiseAdd(b);
+//         })
+//         .def("__mul__", [](const Tensor& a, const Tensor& b) {
+//             return a.elementwiseMultiply(b);
+//         });
+    
+//     // Layer base class
+//     py::class_<Layer>(m, "Layer")
+//         .def("getName", &Layer::getName)
+//         .def("isTrainable", &Layer::isTrainable)
+//         .def("forward", &Layer::forward)
+//         .def("backward", &Layer::backward)
+//         .def("getParameters", &Layer::getParameters)
+//         .def("getGradients", &Layer::getGradients)
+//         .def("zeroGrad", &Layer::zeroGrad);
+    
+//     // Conv2d layer
+//     py::class_<Conv2d, Layer>(m, "Conv2d")
+//         .def(py::init<int, int, int, int, int, std::string>(),
+//              py::arg("in_channels"), py::arg("out_channels"), 
+//              py::arg("kernel_size"), py::arg("stride") = 1,
+//              py::arg("padding") = 0, py::arg("name") = "Conv2d")
+//         .def("getNumParameters", &Conv2d::getNumParameters)
+//         .def("getMACs", &Conv2d::getMACs)
+//         .def("getFLOPs", &Conv2d::getFLOPs);
+    
+//     // MaxPool2d layer
+//     py::class_<MaxPool2d, Layer>(m, "MaxPool2d")
+//         .def(py::init<int, int, int, std::string>(),
+//              py::arg("kernel_size") = 2, py::arg("stride") = 2,
+//              py::arg("padding") = 0, py::arg("name") = "MaxPool2d")
+//         .def("getFLOPs", &MaxPool2d::getFLOPs);
+    
+//     // Linear layer
+//     py::class_<Linear, Layer>(m, "Linear")
+//         .def(py::init<int, int, std::string>(),
+//              py::arg("in_features"), py::arg("out_features"),
+//              py::arg("name") = "Linear")
+//         .def("getNumParameters", &Linear::getNumParameters)
+//         .def("getMACs", &Linear::getMACs)
+//         .def("getFLOPs", &Linear::getFLOPs);
+    
+//     // ReLU layer
+//     py::class_<ReLU, Layer>(m, "ReLU")
+//         .def(py::init<std::string>(), py::arg("name") = "ReLU");
+    
+//     // Utility functions
+//     m.def("crossEntropyLoss", &utils::crossEntropyLoss);
+//     m.def("mseLoss", &utils::mseLoss);
+//     m.def("accuracy", &utils::accuracy);
+//     m.def("normalizeImage", &utils::normalizeImage);
+//     m.def("isCUDAAvailable", &utils::isCUDAAvailable);
+//     m.def("setDevice", &utils::setDevice);
+//     m.def("cudaSynchronize", &utils::cudaSynchronize);
+    
+//     // Timer class
+//     py::class_<utils::Timer>(m, "Timer")
+//         .def(py::init<>())
+//         .def("start", &utils::Timer::start)
+//         .def("elapsed", &utils::Timer::elapsed);
+    
+//     // ModelStats struct
+//     py::class_<utils::ModelStats>(m, "ModelStats")
+//         .def(py::init<>())
+//         .def_readwrite("total_parameters", &utils::ModelStats::total_parameters)
+//         .def_readwrite("total_macs", &utils::ModelStats::total_macs)
+//         .def_readwrite("total_flops", &utils::ModelStats::total_flops)
+//         .def_readwrite("memory_mb", &utils::ModelStats::memory_mb);
+// }
+
+
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <pybind11/numpy.h>
+#include "tensor.h"
+#include "layers.h"
+#include "optimizers.h"
+#include "utils.h"
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(custom_dl_framework, m) {
+    m.doc() = "Custom Deep Learning Framework";
+    
+    // Tensor class
+    py::class_<Tensor>(m, "Tensor")
+        .def(py::init<>())
+        .def(py::init<const std::vector<int>&>(), 
+             py::arg("shape"))
+        .def(py::init<const std::vector<int>&, bool>(), 
+             py::arg("shape"), py::arg("requires_grad") = false)
+        .def(py::init<const std::vector<float>&, const std::vector<int>&>(), 
+             py::arg("data"), py::arg("shape"))
+        .def(py::init<const std::vector<float>&, const std::vector<int>&, bool>(),
+             py::arg("data"), py::arg("shape"), py::arg("requires_grad") = false)
+        .def("getShape", &Tensor::getShape)
+        .def("getData", &Tensor::getData)
+        .def("getTotalSize", &Tensor::getTotalSize)
+        .def("zeros", &Tensor::zeros)
+        .def("ones", &Tensor::ones)
+        .def("randomNormal", &Tensor::randomNormal, 
+             py::arg("mean") = 0.0f, py::arg("stddev") = 1.0f)
+        .def("randomUniform", &Tensor::randomUniform,
+             py::arg("low") = 0.0f, py::arg("high") = 1.0f)
+        .def("xavierUniform", &Tensor::xavierUniform)
+        .def("matmul", &Tensor::matmul)
+        .def("conv2d", &Tensor::conv2d)
+        .def("maxPool2d", &Tensor::maxPool2d)
+        .def("relu", &Tensor::relu)
+        .def("sigmoid", &Tensor::sigmoid)
+        .def("softmax", &Tensor::softmax, py::arg("axis") = -1)
+        .def("reshape", &Tensor::reshape)
+        .def("flatten", &Tensor::flatten)
+        .def("backward", &Tensor::backward)
+        .def("zeroGrad", &Tensor::zeroGrad)
+        .def("__repr__", &Tensor::toString)
+        .def("__add__", [](const Tensor& a, const Tensor& b) {
+            return a.elementwiseAdd(b);
+        })
+        .def("__mul__", [](const Tensor& a, const Tensor& b) {
+            return a.elementwiseMultiply(b);
+        });
+    
+    // Layer base class
+    py::class_<Layer>(m, "Layer")
+        .def("getName", &Layer::getName)
+        .def("isTrainable", &Layer::isTrainable)
+        .def("forward", &Layer::forward)
+        .def("backward", &Layer::backward)
+        .def("getParameters", &Layer::getParameters)
+        .def("getGradients", &Layer::getGradients)
+        .def("zeroGrad", &Layer::zeroGrad);
+    
+    // Conv2d layer
+    py::class_<Conv2d, Layer>(m, "Conv2d")
+        .def(py::init<int, int, int, int, int, std::string>(),
+             py::arg("in_channels"), py::arg("out_channels"), 
+             py::arg("kernel_size"), py::arg("stride") = 1,
+             py::arg("padding") = 0, py::arg("name") = "Conv2d")
+        .def("getNumParameters", &Conv2d::getNumParameters)
+        .def("getMACs", &Conv2d::getMACs)
+        .def("getFLOPs", &Conv2d::getFLOPs);
+    
+    // MaxPool2d layer
+    py::class_<MaxPool2d, Layer>(m, "MaxPool2d")
+        .def(py::init<int, int, int, std::string>(),
+             py::arg("kernel_size") = 2, py::arg("stride") = 2,
+             py::arg("padding") = 0, py::arg("name") = "MaxPool2d")
+        .def("getFLOPs", &MaxPool2d::getFLOPs);
+    
+    // Linear layer
+    py::class_<Linear, Layer>(m, "Linear")
+        .def(py::init<int, int, std::string>(),
+             py::arg("in_features"), py::arg("out_features"),
+             py::arg("name") = "Linear")
+        .def("getNumParameters", &Linear::getNumParameters)
+        .def("getMACs", &Linear::getMACs)
+        .def("getFLOPs", &Linear::getFLOPs);
+    
+    // ReLU layer
+    py::class_<ReLU, Layer>(m, "ReLU")
+        .def(py::init<std::string>(), py::arg("name") = "ReLU");
+    
+    // Utility functions
+    m.def("crossEntropyLoss", &utils::crossEntropyLoss);
+    m.def("mseLoss", &utils::mseLoss);
+    m.def("accuracy", &utils::accuracy);
+    m.def("normalizeImage", &utils::normalizeImage);
+    m.def("isCUDAAvailable", &utils::isCUDAAvailable);
+    m.def("setDevice", &utils::setDevice);
+    m.def("cudaSynchronize", &utils::cudaSynchronize);
+    
+    // Timer class
+    py::class_<utils::Timer>(m, "Timer")
+        .def(py::init<>())
+        .def("start", &utils::Timer::start)
+        .def("elapsed", &utils::Timer::elapsed);
+    
+    // ModelStats struct
+    py::class_<utils::ModelStats>(m, "ModelStats")
+        .def(py::init<>())
+        .def_readwrite("total_parameters", &utils::ModelStats::total_parameters)
+        .def_readwrite("total_macs", &utils::ModelStats::total_macs)
+        .def_readwrite("total_flops", &utils::ModelStats::total_flops)
+        .def_readwrite("memory_mb", &utils::ModelStats::memory_mb);
+}
---
+++ src/cpp/utils.cpp
@@ -0,0 +1,173 @@
+
+#include "utils.h"
+#include "layers.h"  // Need this for Layer class implementation
+#include <cmath>
+#include <chrono>
+
+#ifdef HAS_CUDA
+#include <cuda_runtime.h>
+#endif
+
+namespace utils {
+
+float crossEntropyLoss(const Tensor& predictions, const Tensor& targets) {
+    const auto& pred_data = predictions.getData();
+    const auto& target_data = targets.getData();
+    
+    if (pred_data.size() != target_data.size()) {
+        throw std::runtime_error("Size mismatch in crossEntropyLoss");
+    }
+    
+    float loss = 0.0f;
+    int batch_size = predictions.getShape()[0];
+    int num_classes = predictions.getShape()[1];
+    
+    for (int i = 0; i < batch_size; ++i) {
+        for (int j = 0; j < num_classes; ++j) {
+            int idx = i * num_classes + j;
+            float p = std::max(pred_data[idx], 1e-8f);
+            loss -= target_data[idx] * std::log(p);
+        }
+    }
+    
+    return loss / batch_size;
+}
+
+float mseLoss(const Tensor& predictions, const Tensor& targets) {
+    const auto& pred_data = predictions.getData();
+    const auto& target_data = targets.getData();
+    
+    if (pred_data.size() != target_data.size()) {
+        throw std::runtime_error("Size mismatch in mseLoss");
+    }
+    
+    float loss = 0.0f;
+    for (size_t i = 0; i < pred_data.size(); ++i) {
+        float diff = pred_data[i] - target_data[i];
+        loss += diff * diff;
+    }
+    
+    return loss / pred_data.size();
+}
+
+float accuracy(const Tensor& predictions, const Tensor& targets) {
+    const auto& pred_data = predictions.getData();
+    const auto& target_data = targets.getData();
+    
+    int batch_size = predictions.getShape()[0];
+    int num_classes = predictions.getShape()[1];
+    
+    int correct = 0;
+    for (int i = 0; i < batch_size; ++i) {
+        int pred_class = 0;
+        float max_prob = -1e9;
+        
+        // Find predicted class
+        for (int j = 0; j < num_classes; ++j) {
+            int idx = i * num_classes + j;
+            if (pred_data[idx] > max_prob) {
+                max_prob = pred_data[idx];
+                pred_class = j;
+            }
+        }
+        
+        // Find true class
+        int true_class = 0;
+        for (int j = 0; j < num_classes; ++j) {
+            int idx = i * num_classes + j;
+            if (target_data[idx] > 0.5f) {  // Assuming one-hot encoding
+                true_class = j;
+                break;
+            }
+        }
+        
+        if (pred_class == true_class) {
+            ++correct;
+        }
+    }
+    
+    return static_cast<float>(correct) / batch_size;
+}
+
+ModelStats analyzeModel(const std::vector<Layer*>& layers, 
+                       const std::vector<int>& input_shape) {
+    ModelStats stats = {0, 0, 0, 0.0f};
+    
+    // Simple analysis
+    for (Layer* layer : layers) {
+        if (auto conv = dynamic_cast<Conv2d*>(layer)) {
+            stats.total_parameters += conv->getNumParameters();
+            stats.total_macs += conv->getMACs(input_shape);
+            stats.total_flops += conv->getFLOPs(input_shape);
+        } else if (auto linear = dynamic_cast<Linear*>(layer)) {
+            stats.total_parameters += linear->getNumParameters();
+            stats.total_macs += linear->getMACs(input_shape);
+            stats.total_flops += linear->getFLOPs(input_shape);
+        }
+    }
+    
+    stats.memory_mb = stats.total_parameters * 4.0f / (1024 * 1024);
+    return stats;
+}
+
+Tensor normalizeImage(const Tensor& image, float mean, float std) {
+    std::vector<int> shape = image.getShape();
+    const auto& input_data = image.getData();
+    std::vector<float> output_data(input_data.size());
+    
+    for (size_t i = 0; i < input_data.size(); ++i) {
+        output_data[i] = (input_data[i] - mean) / std;
+    }
+    
+    return Tensor(output_data, shape);
+}
+
+// In utils.cpp, update isCUDAAvailable() function:
+bool isCUDAAvailable() {
+#ifdef HAS_CUDA
+    int device_count = 0;
+    cudaError_t error = cudaGetDeviceCount(&device_count);
+    if (error != cudaSuccess) {
+        // CUDA driver not installed or not compatible
+        return false;
+    }
+    return (device_count > 0);
+#else
+    return false;
+#endif
+}
+
+// bool isCUDAAvailable() {
+// #ifdef HAS_CUDA
+//     int device_count = 0;
+//     cudaError_t error = cudaGetDeviceCount(&device_count);
+//     return (error == cudaSuccess && device_count > 0);
+// #else
+//     return false;
+// #endif
+// }
+
+void setDevice(int device_id) {
+#ifdef HAS_CUDA
+    cudaSetDevice(device_id);
+#endif
+}
+
+void cudaSynchronize() {
+#ifdef HAS_CUDA
+    cudaDeviceSynchronize();
+#endif
+}
+
+// Timer implementation
+void Timer::start() {
+    start_time = std::chrono::high_resolution_clock::now();
+}
+
+double Timer::elapsed() const {
+    auto end_time = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = end_time - start_time;
+    return elapsed.count();
+}
+
+} // namespace utils
